a3e62eeca0d7bb4d0a5df5d45832ca0ef9cc9ac0
Tyler <thowes@student.neumont.edu>
2014-05-19 12:54:15 -0600

    Landing page check boxes and result page done.

--- src/edu/neumont/diyauto/Servlet.java
+++ src/edu/neumont/diyauto/Servlet.java
@@ -1,6 +1,8 @@
 package edu.neumont.diyauto;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,8 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/*")
+@WebServlet("")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
@@ -19,16 +20,30 @@ public class Servlet extends HttpServlet {
     
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/index.jsp");
 		view.forward(request, response);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		request.setAttribute("list", "");
-//		
-//		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/LandingPage.jsp");
-//		view.forward(request, response);
+		String prob1 = request.getParameter("problem1");
+		String prob2 = request.getParameter("problem2");
+		String prob3 = request.getParameter("problem3");
+		String prob4 = request.getParameter("problem4");
+		String prob5 = request.getParameter("problem5");
+		
+		ArrayList<String> values = new ArrayList<String>();
+		values.add(prob1);
+		values.add(prob2);
+		values.add(prob3);
+		values.add(prob4);
+		values.add(prob5);
+		
+		request.setAttribute("list", values);
+		
+		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/GeneratedSolution.jsp");
+		view.forward(request, response);
 	}
 
 }
