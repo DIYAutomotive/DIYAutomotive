478eb29544026f79d4f9c1b52259677d363bbadb
Jonathan Jensen <haiyun211@gmail.com>
2014-05-21 13:17:08 -0600

    Update

--- src/main/java/edu/neumont/diyauto/Servlet.java
+++ src/main/java/edu/neumont/diyauto/Servlet.java
@@ -40,7 +40,7 @@ public class Servlet extends HttpServlet {
 		
 		request.setAttribute("list", values);
 		
-		RequestDispatcher view = request.getRequestDispatcher("WEB_INF/GeneratedSolution.jsp");
+		RequestDispatcher view = request.getRequestDispatcher("GeneratedSolution.jsp");
 		view.forward(request, response);
 	}
 
